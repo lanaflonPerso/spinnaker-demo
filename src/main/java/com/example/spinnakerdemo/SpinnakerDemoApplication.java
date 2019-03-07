305538c3720fc8b1088b9610bb67a23563c40287
papicella <papicella@pivotal.io>
2019-03-07 16:12:05 +1100

    Added support for Pushgateway

--- src/main/java/com/example/spinnakerdemo/SpinnakerDemoApplication.java
+++ src/main/java/com/example/spinnakerdemo/SpinnakerDemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.spinnakerdemo;
 
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class SpinnakerDemoApplication {
